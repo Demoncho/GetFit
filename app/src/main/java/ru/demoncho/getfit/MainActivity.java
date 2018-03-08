593ef720d8fc8bb83d638ee99bc193389e5ba2ef
Demoncho <demon26007@mail.ru>
2018-03-08 20:15:06 +0300

    Добавил ф-ию gif анимации

--- app/src/main/java/ru/demoncho/getfit/MainActivity.java
+++ app/src/main/java/ru/demoncho/getfit/MainActivity.java
@@ -1,5 +1,6 @@
 package ru.demoncho.getfit;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.webkit.WebView;
+
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -20,6 +23,7 @@ public class MainActivity extends AppCompatActivity
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
